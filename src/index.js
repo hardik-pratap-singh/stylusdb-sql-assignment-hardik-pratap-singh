05b6d05fb77e7ae8346bcd36109dcc4424d20880
hardik-pratap-singh <21bcs090@iiitdmj.ac.in>
2024-04-11 12:23:16 +0530

    updated test:7

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
+const { readCSV, writeCSV } = require('./csvReader');
+const { parseSelectQuery, parseInsertQuery, parseDeleteQuery } = require('./queryParser');
+const { executeSELECTQuery, executeINSERTQuery, executeDELETEQuery } = require('./queryExecuter');
 
-const parseQuery = require('./queryParser');
-const readCSV = require('./csvReader');
-
-async function executeSELECTQuery(query) {
-    try {
-        const { fields, table } = parseQuery(query);
-        const data = await readCSV(`${table}.csv`);
-        
-        // Filter the fields based on the query
-        return data.map(row => {
-            const filteredRow = {};
-            fields.forEach(field => {
-                filteredRow[field] = row[field];
-            });
-            return filteredRow;
-        });
-    } catch (error) {
-        // Handle file not found error
-        console.error("Error: File not found or table does not exist");
-        return [];
-    }
+module.exports = {
+    readCSV,
+    writeCSV,
+    executeSELECTQuery,
+    executeINSERTQuery,
+    executeDELETEQuery,
+    parseSelectQuery,
+    parseInsertQuery,
+    parseDeleteQuery
 }
-
-module.exports = executeSELECTQuery;
